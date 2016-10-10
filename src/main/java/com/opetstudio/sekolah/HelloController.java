46366d8b22e3d432e5a19e286732591c5a5a8d10
Nofrets <opetstudio@gmail.com>
2016-10-10 17:52:21 +0700

    edit untuk animasi

--- src/main/java/com/opetstudio/sekolah/HelloController.java
+++ src/main/java/com/opetstudio/sekolah/HelloController.java
@@ -1,7 +1,10 @@
 package com.opetstudio.sekolah;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +14,17 @@ import org.slf4j.LoggerFactory;
 public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
     public Label helloWorld;
+    //The @FXML annotation is used to tag nonpublic controller member fields and handler methods for use by FXML markup.
+    @FXML private Text actiontarget;
 
     public void sayHelloWorld(ActionEvent actionEvent) {
         log.debug("sayHelloWorld");
         helloWorld.setText("Hellow World!");
     }
 
-    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    @FXML protected void handleSubmitButtonAction(ActionEvent actionEvent) {
+        log.debug("signin submited");
+        actiontarget.setFill(Color.FIREBRICK);
+        actiontarget.setText("Sign in button pressed");
     }
 }
